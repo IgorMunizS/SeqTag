b12b7b4508157992ce38c276ec8c60559a82a197
Muniz Soares, Igor <imunizs@indracompany.com>
2019-12-03 16:47:02 -0300

    new tokenizer

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -8,12 +8,12 @@ from tqdm import tqdm
 
 def tokenize(X_train,X_test,max_features, maxlen):
     print("Tokenizando")
-    tok = text.Tokenizer(num_words=max_features, lower=True)
+    tok = text.Tokenizer(num_words=max_features, lower=True, filters=None, oov_token='outro')
     tok.fit_on_texts(list(X_train) + list(X_test))
     X_train = tok.texts_to_sequences(X_train)
     X_test = tok.texts_to_sequences(X_test)
-    X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
-    X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+    X_train = sequence.pad_sequences(X_train, maxlen=maxlen, padding='post', value=0)
+    X_test = sequence.pad_sequences(X_test, maxlen=maxlen, padding='post', value=0)
     # # saving
     # with open('../tokenizers/' + lang + '_tokenizer.pickle', 'wb') as handle:
     #     pickle.dump(X_test, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- utils/utils.py
+++ utils/utils.py
@@ -6,8 +6,8 @@ def pad_tokens(y_train, maxlen, label_encoder):
     padded_y_train = np.zeros((len(y_train), maxlen), dtype=np.int)
     for i,sample in enumerate(y_train):
         pad = [config.pad_seq_tag]*(maxlen - len(sample))
-        pad.extend(sample)
-        padded_y_train[i] = label_encoder.transform(pad)
+        sample.extend(pad)
+        padded_y_train[i] = label_encoder.transform(sample)
     return padded_y_train
 
 def per_class_accuracy(y_preds,y_true):
