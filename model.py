9ff4b05e244720e1a919bc5b1cc4a01fdf104d35
Muniz Soares, Igor <imunizs@indracompany.com>
2019-12-03 19:39:13 -0300

    mask zero in training

--- model.py
+++ model.py
@@ -8,8 +8,8 @@ def get_model(max_len,max_features,embed_size,embedding_matrix,num_tags):
 
     embedding = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False, mask_zero=True)(sequence_input)
     x = SpatialDropout1D(0.3)(embedding)
-    x1 = Bidirectional(CuDNNLSTM(256, return_sequences=True))(x)
-    x2 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x1)
+    x1 = Bidirectional(LSTM(256, return_sequences=True))(x)
+    x2 = Bidirectional(LSTM(128, return_sequences=True))(x1)
     td = Dense(50, activation="relu")(x2)
     crf = CRF(num_tags)  # CRF layer
     out = crf(td)  # output
