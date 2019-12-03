785637111ab206ac52b1459d741fc2190fc66b15
Muniz Soares, Igor <imunizs@indracompany.com>
2019-12-03 19:44:55 -0300

    mask zero in training

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from keras.callbacks import ModelCheckpoint
 def get_model(max_len,max_features,embed_size,embedding_matrix,num_tags):
     sequence_input = Input(shape=(max_len,))
 
-    embedding = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False, mask_zero=True)(sequence_input)
+    embedding = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False, mask_zero=False)(sequence_input)
     x = SpatialDropout1D(0.3)(embedding)
     x1 = Bidirectional(LSTM(256, return_sequences=True))(x)
     x2 = Bidirectional(LSTM(128, return_sequences=True))(x1)
