a6d2f6985a2cc3f747c05853ff34656166423be7
Muniz Soares, Igor <imunizs@indracompany.com>
2019-12-04 08:51:23 -0300

    add callbacks to calculate bacc

--- model.py
+++ model.py
@@ -2,11 +2,17 @@ from keras.layers import LSTM, Dense, TimeDistributed, Embedding, Bidirectional,
 from keras.models import Model, Input
 from anago.layers import CRF
 from keras.callbacks import ModelCheckpoint
-
+import config
 def get_model(max_len,max_features,embed_size,embedding_matrix,num_tags):
     sequence_input = Input(shape=(max_len,))
 
-    embedding = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=True, mask_zero=True)(sequence_input)
+    embedding = Embedding(
+        max_features,
+        embed_size,
+        weights=[embedding_matrix],
+        trainable=config.train_embeddings,
+        mask_zero=True)(sequence_input)
+
     x = Dropout(0.5)(embedding)
     x1 = Bidirectional(LSTM(100, return_sequences=True))(x)
     td = Dense(100, activation='tanh')(x1)
