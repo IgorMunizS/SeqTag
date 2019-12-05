c15afdbfd9e4e09f87095f1b86f36671a161cc67
Muniz Soares, Igor <imunizs@indracompany.com>
2019-12-05 14:35:50 -0300

    add evaluate and predict kfold

--- evaluation.py
+++ evaluation.py
@@ -1,36 +1,46 @@
-from utils.generator import DataGenerator
 import numpy as np
 import config
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+import pandas as pd
+from ast import literal_eval
+from anago.preprocessing import IndexTransformer
+from keras.models import load_model
+from anago.layers import CRF
 
-def evaluate(model,X_train,y_train, tags, label_encoder):
+def evaluate():
+    train = pd.read_csv(config.data_folder + "train.csv", converters={"pos": literal_eval, "tag": literal_eval})
+    x_train = [x.split() for x in train['sentence'].tolist()]
+    y_train = train['tag'].tolist()
 
-    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, train_size=0.9, random_state=233)
+    p = IndexTransformer(use_char=True)
+    p.load('../models/best_transform.it')
 
-    val_generator = DataGenerator(X_test[:10240], y_test[:10240], tags, batch_size=128, shuffle=False)
+    oof_data = []
+    oof_data_pred = []
 
-    y_pred = model.predict_generator(
-        val_generator,
-        workers=40,
-        verbose=1
-    )
+    skf = KFold(n_splits=config.nfolds, random_state=config.seed, shuffle=True)
 
-    y_pred = np.argmax(y_pred, axis=-1)
-    # y_test = np.argmax(y_test, axis=-1)
+    for n_fold, (train_indices, val_indices) in enumerate(skf.split(x_train)):
+        x_val = list(np.array(x_train)[val_indices])
+        y_val = list(np.array(y_train)[val_indices])
 
-    y_pred = y_pred.flatten()
-    y_test = y_test[:10240].flatten()
-    print(y_pred.shape)
-    print(y_test.shape)
-    y_pred = label_encoder.inverse_transform(y_pred)
-    y_test = label_encoder.inverse_transform(y_test)
-
-    y_pred = y_pred[y_pred != config.pad_seq_tag]
-    y_test = y_test[y_test != config.pad_seq_tag]
+        lengths = map(len, x_val)
+        x_val = p.transform(x_val)
 
 
-    print("ACC-score is : {:.1%}".format(accuracy_score(y_test, y_pred)))
-    print("BACC-score is : {:.1%}".format(balanced_accuracy_score(y_test, y_pred)))
-    print("F1-score is : {:.1%}".format(f1_score(y_test, y_pred)))
+        oof_data.extend([x for line in x_val for x in line])
+
+
+
+        model = load_model('../models/best_model_' + str(n_fold) + '.h5', custom_objects={'CRF': CRF})
+        y_pred = model.predict(x_val,
+                               verbose=True)
+        y_pred = p.inverse_transform(y_pred, lengths)
+        oof_data_pred.extend([pred for line in y_pred for pred in line])
+
+    bacc = balanced_accuracy_score(oof_data,oof_data_pred)
+    print("Final CV: ", bacc*100)
+
+
 
--- predict.py
+++ predict.py
@@ -3,6 +3,9 @@ import config
 from anago.preprocessing import IndexTransformer
 from anago.models import BiLSTMCRF
 from ast import literal_eval
+from anago.layers import CRF
+import numpy as np
+from keras.models import load_model
 
 
 def build_submission(y_pred, n_fold):
@@ -46,6 +49,31 @@ def load_and_predict():
     model.load_weights('../models/' + 'best_model.h5')
 
     predict(model, p, x_test)
+
+def predict_with_folds():
+    test = pd.read_csv(config.data_folder + "test.csv", converters={"pos": literal_eval})
+    x_test = [x.split() for x in test['sentence'].tolist()]
+
+    p = IndexTransformer(use_char=True)
+    p.load('../models/best_transform.it')
+    lengths = map(len, x_test)
+    x_test = p.transform(x_test)
+
+    fold_result = []
+    for n_fold in range(config.nfolds):
+
+
+        model = load_model('../models/best_model_' + str(n_fold) + '.h5', custom_objects={'CRF': CRF})
+        y_pred = model.predict(x_test,
+                               verbose=True)
+
+        fold_result.append(y_pred)
+
+    final_pred = np.mean(fold_result, axis=0)
+    y_pred = p.inverse_transform(final_pred, lengths)
+    build_submission(y_pred, 'fold')
+
+
 
 if __name__ == '__main__':
    load_and_predict()
