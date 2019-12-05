47bd4010e11e1c77e7699dc5135c38ddc7fe45ab
Muniz Soares, Igor <imunizs@indracompany.com>
2019-12-05 10:07:23 -0300

    fix kfold

--- anago/layers.py
+++ anago/layers.py
@@ -49,14 +49,14 @@ def crf_nll(y_true, y_pred):
 
     # multiply this by the inputs:
     # maybe you might need K.expand_dims(isMask) to add the extra dimension removed by K.all
-    y_true = y_true * isMask
-    crf = crf * isMask
+    # y_true = y_true * isMask
+    # crf = crf * isMask
     if crf._outbound_nodes:
         raise TypeError('When learn_model="join", CRF must be the last layer.')
     if crf.sparse_target:
         y_true = K.one_hot(K.cast(y_true[:, :, 0], 'int32'), crf.units)
     X = crf._inbound_nodes[idx].input_tensors[0]
-    mask = crf._inbound_nodes[idx].input_masks[0]
+    mask = isMask
     nloglik = crf.get_negative_log_likelihood(y_true, X, mask)
     return nloglik
 
